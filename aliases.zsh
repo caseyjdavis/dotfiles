c66ce527a1e3467bcd71347e9ed9dbbab7238d0d
Casey Davis <casey.j.davis@gmail.com>
2025-03-17 08:49:20 -0600

    tweaked tmux and aliases

--- aliases.zsh
+++ aliases.zsh
@@ -1,3 +1,3 @@
 alias reload!='. ~/.zshrc'
 alias ls='ls -lah'
-alias pubkey="more ~/.ssh/id_rsa.pub | pbcopy | echo '=> Public key copied to pasteboard.'"
+alias pubkey="more ~/.ssh/id_ed25519.pub | pbcopy | echo '=> Public key copied to pasteboard.'"
